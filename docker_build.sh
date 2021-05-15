15e22f440c4fc53e7b02b623204f00128f320ad1
Diablo2050 <amrmrashad@gmail.com>
2021-05-15 08:50:36 +0200

    using tag latest

--- docker_build.sh
+++ docker_build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-docker build -t diablo2050/capestone:${CIRCLE_WORKFLOW_ID:0:7} .
+# docker build -t diablo2050/capestone:${CIRCLE_WORKFLOW_ID:0:7} .
+docker build -t diablo2050/capestone:latest .
