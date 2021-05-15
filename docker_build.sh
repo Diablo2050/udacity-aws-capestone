fb7994db0a889fa3a6b3d0d40d060a71767c411f
Diablo2050 <amrmrashad@gmail.com>
2021-05-15 09:43:49 +0200

    update for rolling update

--- docker_build.sh
+++ docker_build.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-# docker build -t diablo2050/capestone:${CIRCLE_WORKFLOW_ID:0:7} .
-docker build -t diablo2050/capestone:latest .
+docker build -t diablo2050/capestone:${CIRCLE_WORKFLOW_ID:0:7} .
